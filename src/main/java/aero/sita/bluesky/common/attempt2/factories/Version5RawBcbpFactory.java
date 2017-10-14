3b92849d94ccb35f2584d1c8dd51f9200059cabb
Ben Craig <ben.craig@sita.aero>
2017-10-14 12:10:53 +0100

    Start fillin gout the unit tests. Note - probably going to change from a list to a map as a list is to awkward to retrieve data out of.

--- src/main/java/aero/sita/bluesky/common/attempt2/factories/Version5RawBcbpFactory.java
+++ src/main/java/aero/sita/bluesky/common/attempt2/factories/Version5RawBcbpFactory.java
@@ -2,19 +2,35 @@ package aero.sita.bluesky.common.attempt2.factories;
 
 import aero.sita.bluesky.common.attempt2.RawBcbp;
 import aero.sita.bluesky.common.attempt2.RawBcbpFieldParserDecorator;
-import aero.sita.bluesky.common.attempt2.fields.mandatory.FormatCode;
+import aero.sita.bluesky.common.attempt2.fields.mandatory.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Version5RawBcbpFactory implements RawBcbpFactory {
 
+    // TODO: I think a map would be better for this; easier to retrieve stuff out of it later.
     List<RawBcbpFieldParserDecorator> leaves = new ArrayList<>();
 
 
     // TODO: obviously the commonality between versions will need to be built in some type of common factory
     public Version5RawBcbpFactory() {
+
         leaves.add(new RawBcbpFieldParserDecorator(new FormatCode()));
+        leaves.add(new RawBcbpFieldParserDecorator(new NumberOfLegsEncoded()));
+        leaves.add(new RawBcbpFieldParserDecorator(new PassengerName()));
+        leaves.add(new RawBcbpFieldParserDecorator(new ElectronicTicketIndicator()));
+        leaves.add(new RawBcbpFieldParserDecorator(new OperatingCarrierPnrCode()));
+        leaves.add(new RawBcbpFieldParserDecorator(new FromCityAirportCode()));
+        leaves.add(new RawBcbpFieldParserDecorator(new ToCityAirportCode()));
+        leaves.add(new RawBcbpFieldParserDecorator(new OperatingCarrierDesignator()));
+        leaves.add(new RawBcbpFieldParserDecorator(new FlightNumber()));
+        leaves.add(new RawBcbpFieldParserDecorator(new DateOfFlight()));
+        leaves.add(new RawBcbpFieldParserDecorator(new CompartmentCode()));
+        leaves.add(new RawBcbpFieldParserDecorator(new SeatNumber()));
+        leaves.add(new RawBcbpFieldParserDecorator(new CheckinSequenceNumber()));
+        leaves.add(new RawBcbpFieldParserDecorator(new PassengerStatus()));
+        leaves.add(new RawBcbpFieldParserDecorator(new FieldSizeOfVaraibleSizeField()));
     }
 
 
--- src/test/java/aero/sita/bluesky/common/attempt2/RawBcbpIntegrationTest.java
+++ src/test/java/aero/sita/bluesky/common/attempt2/RawBcbpIntegrationTest.java
@@ -3,20 +3,39 @@ package aero.sita.bluesky.common.attempt2;
 import aero.sita.bluesky.common.attempt2.factories.RawBcbpFactory;
 import aero.sita.bluesky.common.attempt2.factories.Version5RawBcbpFactory;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RawBcbpIntegrationTest {
 
-    private RawBcbpFactory factory = new Version5RawBcbpFactory();
+    private RawBcbpFactory factory;
+    private RawBcbp fixture;
+    private String result;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new Version5RawBcbpFactory();
+        fixture = factory.create();
+        result = fixture.consume("M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 226F001A0025 100");
+    }
 
     @Test
     public void shouldDoSimpleParse() {
-        RawBcbp fixture = factory.create();
-
-        String result = fixture.consume("ABC");
-
-        Assert.assertEquals("FormatCode", fixture.getLeaves().get(0).getName());
-        Assert.assertEquals("A", fixture.getLeaves().get(0).getContents());
-        Assert.assertEquals("BC", result);
+        Assert.assertEquals("", result);
+        Assert.assertEquals(15, fixture.getLeaves().size());
     }
+
+    @Test
+    public void shouldParseFormatCode() {
+        Assert.assertEquals("FormatCode", fixture.getLeaves().get(0).getName());
+        Assert.assertEquals("M", fixture.getLeaves().get(0).getContents());
+    }
+
+    @Test
+    public void shouldParseNumberOfLegsEncoded() {
+        Assert.assertEquals("NumberOfLegsEncoded", fixture.getLeaves().get(1).getName());
+        Assert.assertEquals("1", fixture.getLeaves().get(1).getContents());
+    }
+
+
 }

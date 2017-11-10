c05d37078fb54dfc342cde19afc9e619f4d86f9c
Ben Craig <bcraig83@gmail.com>
2017-11-10 23:18:07 +0000

    Start buidling out the first-pass parser.

--- src/main/java/b3ls/FirstPassBcbpParserFactory.java
+++ src/main/java/b3ls/FirstPassBcbpParserFactory.java
@@ -1,9 +1,17 @@
 package b3ls;
 
 public class FirstPassBcbpParserFactory implements BcbpParserFactory {
+
+
     @Override
     public SimpleTextParser create() {
-        SimpleTextParser result = new SimpleTextParser();
-        return result;
+        return new SimpleTextParser()
+                .addRawGroupParser(new ParseableRawGroup(new RawGroup("BasicInfo")
+                        .addRawFieldParser(new ParseableRawField(new RawField(1, "FormatCode")))
+                        .addRawFieldParser(new ParseableRawField(new RawField(1, "NumberOfLegsEncoded")))
+                        .addRawFieldParser(new ParseableRawField(new RawField(56, "StuffWeDontCareAbout")))
+                        .addRawFieldParser(new ParseableRawField(new RawField(2, "FieldSizeOfVaribleSizeField")))));
+
+        // TODO: add everything else
     }
 }

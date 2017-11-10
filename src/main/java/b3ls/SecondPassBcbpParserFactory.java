4258834902df6ca440352632e6ef00e5b6c6176a
Ben Craig <bcraig83@gmail.com>
2017-11-10 23:09:19 +0000

    Put some dummy unit tests in

--- src/main/java/b3ls/SecondPassBcbpParserFactory.java
+++ src/main/java/b3ls/SecondPassBcbpParserFactory.java
@@ -3,6 +3,7 @@ package b3ls;
 public class SecondPassBcbpParserFactory implements BcbpParserFactory {
     @Override
     public SimpleTextParser create() {
-        return null;
+        SimpleTextParser result = new SimpleTextParser();
+        return result;
     }
 }
--- src/test/java/b3ls/SecondPassBcbpParserFactoryTest.java
+++ src/test/java/b3ls/SecondPassBcbpParserFactoryTest.java
@@ -1,4 +1,15 @@
 package b3ls;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SecondPassBcbpParserFactoryTest {
+
+    @Test
+    public void shouldBuildSomething() {
+        BcbpParserFactory factory = new SecondPassBcbpParserFactory();
+        SimpleTextParser result = factory.create();
+
+        Assert.assertNotNull(result);
+    }
 }

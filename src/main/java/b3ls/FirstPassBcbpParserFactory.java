a6dbcc89eb9466a89e209382388ad11ffe498b4e
Ben Craig <bcraig83@gmail.com>
2017-11-10 23:07:58 +0000

    Rename

--- src/main/java/b3ls/FirstPassBcbpParserFactory.java
+++ src/main/java/b3ls/FirstPassBcbpParserFactory.java
@@ -1,6 +1,6 @@
 package b3ls;
 
-public class StandardBcbpParserFactory implements BcbpParserFactory {
+public class FirstPassBcbpParserFactory implements BcbpParserFactory {
     @Override
     public SimpleTextParser create() {
         SimpleTextParser result = new SimpleTextParser();
--- src/test/java/b3ls/FirstPassBcbpParserFactoryTest.java
+++ src/test/java/b3ls/FirstPassBcbpParserFactoryTest.java
@@ -3,11 +3,11 @@ package b3ls;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class BcbpParserFactoryTest {
+public class FirstPassBcbpParserFactoryTest {
 
     @Test
     public void shouldBuildSomething() {
-        BcbpParserFactory factory = new StandardBcbpParserFactory();
+        BcbpParserFactory factory = new FirstPassBcbpParserFactory();
 
         SimpleTextParser result = factory.create();
 

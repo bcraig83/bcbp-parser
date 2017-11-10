eeb4a4d9f16c7040f490cc149377cf506ec22e60
Ben Craig <bcraig83@gmail.com>
2017-11-10 23:03:13 +0000

    Return an empty parser for now.

--- src/test/java/b3ls/BcbpParserFactoryTest.java
+++ src/test/java/b3ls/BcbpParserFactoryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 public class BcbpParserFactoryTest {
 
-
     @Test
     public void shouldBuildSomething() {
         BcbpParserFactory factory = new StandardBcbpParserFactory();
